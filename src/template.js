1ac02cf3eb085f718aaa08befc05d17b60a91064
Tristan Sandras <tristan@sandras.fr>
2016-09-01 10:07:45 +0200

    add template#components

--- src/template.js
+++ src/template.js
@@ -9,47 +9,46 @@ Template = function(game, name, spriteName, text) {
   self.text = self.game.add.text(0, 0, text, { font: "24px Georgia", fill: TEXT_COLOR, align: "center", boundsAlignH: "center", boundsAlignV: "middle", wordWrap: true, wordWrapWidth: 200});
   self.text.setTextBounds(590, 20, 200, 115);
   self.text.blendMode = PIXI.blendModes.COLOR_BURN;
+  self.components = [
+    ['square+raw_1x2', 1, 2, 'Raw'],
+    ['square+raw_1x2', 1, 2, 'Raw'],
+    ['square+raw_2x1', 2, 1, 'Raw'],
+    ['square+raw_2x1', 2, 1, 'Raw'],
+    ['square+raw_1x1', 1, 1, 'Raw'],
+    ['square+raw_1x1', 1, 1, 'Raw'],
+    ['square+raw_2x2', 2, 2, 'Raw']
+  ];
 
   self.spawnsThreeComponents = function(grid) {
-    // Add components (assets)
-    // TODO genericity by level
-    names = [
-      ['square+raw_1x2', 1, 2, 'Raw'],
-      ['square+raw_1x2', 1, 2, 'Raw'],
-      ['square+raw_2x1', 2, 1, 'Raw'],
-      ['square+raw_2x1', 2, 1, 'Raw'],
-      ['square+raw_1x1', 1, 1, 'Raw'],
-      ['square+raw_1x1', 1, 1, 'Raw'],
-      ['square+raw_2x2', 2, 2, 'Raw']
-    ];
     var tmpX = 655;
     var tmpY = 200;
     var rand = 0;
+    var tmpComponent = null;
     for (i = 0; i < 3; i++) {
-      // Pas oublier de changer la valeur = names.length
-      rand = Math.floor(Math.random() * 7);
+      rand = Math.floor(Math.random() * self.components.length);
       tmpX = 655;
+      tmpComponent = self.components[rand];
       // case of 1x2
-      if (names[rand][1] == 1 && names[rand][2] == 2) {
+      if (tmpComponent[1] == 1 && tmpComponent[2] == 2) {
         tmpX = tmpX + 20;
       }
       // case of 2x1
-      if (names[rand][1] == 2 && names[rand][2] == 1) {
+      if (tmpComponent[1] == 2 && tmpComponent[2] == 1) {
         tmpY = tmpY + 20;
       }
       // case of 1x1
-      if (names[rand][1] == 1 && names[rand][2] == 1) {
+      if (tmpComponent[1] == 1 && tmpComponent[2] == 1) {
         tmpY = tmpY + 20;
         tmpX = tmpX + 20;
       }
-      var c = new Component(game, self.index, names[rand][0], tmpX, tmpY, names[rand][1], names[rand][2], names[rand][3], names[rand][4]);
+      var c = new Component(game, self.index, tmpComponent[0], tmpX, tmpY, tmpComponent[1], tmpComponent[2], tmpComponent[3], tmpComponent[4]);
       self.index = self.index + 1;
       c.loadEvents(grid);
       grid.setComponent(c, 12, 3 + 2*i);
 
-      if (names[rand][1] == 1 && names[rand][2] == 1) {
+      if (tmpComponent[1] == 1 && tmpComponent[2] == 1) {
         tmpY = tmpY + 80;
-      } else if (names[rand][1] == 2 && names[rand][2] == 1) {
+      } else if (tmpComponent[1] == 2 && tmpComponent[2] == 1) {
         tmpY = tmpY + 80;
       } else {
         tmpY = tmpY + 100;
