27ee9989a97ebad7aefb8d7263f20d4c3cb4eb34
Tristan Sandras <tristan@sandras.fr>
2016-09-14 16:15:34 +0200

    Add levels menu logic

--- src/levels.js
+++ src/levels.js
@@ -1,5 +1,8 @@
 function Levels(levels) {
   var self = this;
+  self.levelsIcon = [];
+  self.levelsText = [];
+  self.hidden = true;
 
   self.data = game.cache.getJSON('levels');
 
@@ -33,6 +36,72 @@ function Levels(levels) {
       }
     }
     return out;
+  }
+
+  var addLevelIcon = function(tmpX, tmpY, name, image, id) {
+    self.levelsText.push(game.add.text(tmpX, tmpY, name, { font: "16px Georgia", fill: TEXT_COLOR, align: "center", boundsAlignH: "center", boundsAlignV: "middle", wordWrap: true, wordWrapWidth: 200}));
+    var img = game.add.image(tmpX, tmpY, image);
+    img.scale.set(0.25 , 0.25);
+    img.id = id;
+    loadEvents(img);
+    self.levelsIcon.push(img);
+  }
+
+  self.displayLevelsMenu = function() {
+    self.hidden = false;
+    var titleScreen = game.add.image(0, 0, 'ld36_bg_001');
+    var tmpX = 90;
+    var tmpY = 90;
+    for(i = 0; i < self.data.length; i++) {
+      var level = self.data[i];
+      addLevelIcon(tmpX, tmpY, level.levelName, level.winImage, i);
+      if ((i + 1) % 3 == 0) { tmpX = 90; tmpY += 150; } else { tmpX += 150; }
+    }
+  }
+
+  self.hideLevelsMenu = function() {
+    self.hidden = true;
+    if (self.levelsText.length < 1 || self.levelsIcon.length < 1) {
+      return;
+    }
+    for(i = 0; i < self.data.length; i++) {
+      self.levelsText[i].destroy();
+      self.levelsIcon[i].destroy();
+    }
+    self.levelsIcon = [];
+    self.levelsText = [];
+  }
+
+  self.manageLevelsMenu = function() {
+    if (self.hidden) {
+      self.displayLevelsMenu();
+    } else {
+      self.hideLevelsMenu();
+    }
+  }
+
+  var over = function(sprite, pointeur) {
+    var htmlGame = document.getElementById('ld36');
+    htmlGame.style.cursor = "url('assets/ld36_cursor_002.png'), auto";
+  }
+
+  var out = function(sprite, pointeur) {
+    var htmlGame = document.getElementById('ld36');
+    htmlGame.style.cursor = "url('assets/ld36_cursor_001.png'), auto";
+  }
+
+  var listener = function(sprite, pointeur) {
+    id = sprite.id;
+    self.hideLevelsMenu();
+    // index.html function
+    initGame();
+  }
+
+  var loadEvents = function(img) {
+    img.inputEnabled = true;
+    img.events.onInputOver.add(over, this);
+    img.events.onInputOut.add(out, this);
+    img.events.onInputDown.add(listener, this);
   }
 
   // Why doesn't it work?
